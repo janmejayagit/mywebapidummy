3825b86b9bfb1639d5bbd5cba9634bee7f03dec4
janmejayagit <54415656+janmejayagit@users.noreply.github.com>
2019-08-25 18:56:07 +0530

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -43,13 +43,6 @@ namespace APIVScode.Controllers
         public void Delete(int id)
         {
         }
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete1(int id)
-         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put1(int id, [FromBody] string value)
-        {
-        }
+       
     }
 }
